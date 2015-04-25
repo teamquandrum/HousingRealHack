621a18592506b2a2d3ad7173ff9091251f197644
Adithya Abraham Philip <adithyaphilip@gmail.com>
2015-04-26 03:33:25 +0530

    corrected

--- application/models/property.php
+++ application/models/property.php
@@ -22,6 +22,8 @@ class Property extends ExposedModel {
     public function getCentroid($params) {
         $req = ['lats','lons'];
         $this->throwExceptionOnUnset($params, $req);
+        $params['lats'] = explode(":", $params['lats']);
+        $params['lons'] = explode(":", $params['lons']);
         $params['lats'][count('lats')]=$params['lats'][0];
         $params['lons'][count('lons')]=$params['lons'][0];
         return $params; 
