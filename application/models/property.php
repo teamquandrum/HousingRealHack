df3a903c0b9f3b87ba398bf883685264206e6eea
Adithya Abraham Philip <adithyaphilip@gmail.com>
2015-04-26 08:28:11 +0530

    corrected

--- application/models/property.php
+++ application/models/property.php
@@ -20,12 +20,32 @@ class Property extends ExposedModel {
     }
     
     public function getCentroid($params) {
-        $req = ['lats','lons'];
+        $req = ['lats','lons','budget'];
         $this->throwExceptionOnUnset($params, $req);
         $params['lats'] = explode(":", $params['lats']);
         $params['lons'] = explode(":", $params['lons']);
-        $params['lats'][count($params['lats'])]=$params['lats'][0];
-        $params['lons'][count($params['lons'])]=$params['lons'][0];
-        return $params; 
+        $params['lats'][]=$params['lats'][0];
+        $params['lons'][]=$params['lons'][0];
+        
+        // calculate area
+        $area = 0.0;
+        $x=0.0; $y=0.0;
+        for($i=0;$i<count($params['lons'])-1;$i++) {
+            $xi = $params['lats'][$i]; $xi1 = $params['lats'][$i+1];
+            $yi = $params['lons'][$i]; $yi1 = $params['lons'][$i+1];
+            $area+=$xi*$yi1 - $xi1*$yi;
+            $x+=($xi+$xi1)*($xi*$yi1 - $xi1*$yi);
+            $y+=($yi+$yi1)*($xi*$yi1 - $xi1*$yi);
+        }
+        $area/=2;
+        echo $area;
+        if($area==0) {
+           echo $x;
+           echo $y;
+        } else {
+            $x = $x/6/$area;
+            $y = $y/6/$area;
+        }
+        return [$x,$y]; 
     }
 }
