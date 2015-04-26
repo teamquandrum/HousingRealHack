04e2886dafca762b26d062ac9df73c31a9943b35
Adithya Abraham Philip <adithyaphilip@gmail.com>
2015-04-26 10:02:26 +0530

    corrected

--- application/models/user.php
+++ application/models/user.php
@@ -9,23 +9,26 @@ class User extends ExposedModel {
     }
     public function newUser($params) {
         $params['budget'] = 2000;
-        $req = array("name","gender", "openness","conscience","extroverted","agreeable","neurotic","budget");
+        if(!isset($params['phno'])) $params['phno']=  "923".rand (0, 9).rand (0, 9).rand (0, 9).rand (0, 9).rand (0, 9).rand (0, 9).rand (0, 9);
+        $req = array("name","gender", "openness","conscience","extroverted","agreeable","neurotic","budget","phno");
         
         $result['userid'] = $this->newEntry ($params, $req);
         return $result;
     }
     public function getRoommates($params) {
         $req = ['userid','budget','lat','lon'];
-        $user = $this->getEntries($params, ['userid']);
+        $user = $this->getEntries($params, ['userid'])[0];
         $budget = $params['budget'];
         // first look for people with +-2000 rupees budget
         $query = "Select *, "
-                . "POW({$user['openness']}-openness,2)"
-                . "POW({$user['openness']}-openness,2)"
-                . "POW({$user['openness']}-openness,2)"
-                . "POW({$user['openness']}-openness,2)"
-                . "POW({$user['openness']}-openness,2)"
-                . "from user where budget > $budget-2000 AND budget < $budget + 2000";
+                . "POW({$user['openness']}-openness,2)+"
+                . "POW({$user['conscience']}-conscience,2)+"
+                . "POW({$user['neurotic']}-neurotic,2)+"
+                . "POW({$user['agreeable']}-agreeable,2)+"
+                . "POW({$user['extroverted']}-extroverted,2)"
+                . "as score "
+                . "from user where budget > $budget-2000 AND budget < $budget + 2000"
+                        . " order by score";
         return $this->db->query($query)->result_array();
     }
 }
